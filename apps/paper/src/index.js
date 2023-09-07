617d25c060a7cbe905593a9c8255e50144012082
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2023-09-07 15:12:50 +0200

    Add web example (not working)

---
+++ apps/paper/src/index.js
@@ -0,0 +1,7 @@
+import {AppRegistry} from 'react-native';
+import LottieAnimatedExample from './src/LottieAnimatedExample';
+
+AppRegistry.registerComponent('example', () => LottieAnimatedExample);
+AppRegistry.runApplication('example', {
+    rootTag: document.getElementById('root')
+  });
