678214f614e0912e99a7a245640d5279bf3984ee
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2023-09-11 18:32:02 +0200

    test

--- apps/paper/index.web.js
+++ apps/paper/index.web.js
@@ -2,6 +2,7 @@ import {AppRegistry} from 'react-native';
 import LottieAnimatedExample from './src/LottieAnimatedExample';
 
 AppRegistry.registerComponent('example', () => LottieAnimatedExample);
+
 AppRegistry.runApplication('example', {
-    rootTag: document.getElementById('root')
-  });
+  rootTag: document.getElementById('root'),
+});
