323762988883213d7dd606e126e039d0e2bdf59f
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2023-09-12 14:24:17 +0200

    prettier

--- apps/paper/src/LottieAnimatedExample.tsx
+++ apps/paper/src/LottieAnimatedExample.tsx
@@ -1,44 +1,52 @@
-import { Button, SafeAreaView, Text, View } from 'react-native';
+import {Button, SafeAreaView, Text, View} from 'react-native';
 import LottieView from 'lottie-react-native';
-import { useState } from 'react';
+import {useState} from 'react';
 
 const ANIMATIONS = [
   {
     name: 'switch.lottie',
-    path: require("./animations/animation_lkekfrcl.lottie")
+    path: require('./animations/animation_lkekfrcl.lottie'),
   },
   {
     name: 'hero.json',
-    path: require("./animations/hero.json"),
-  }
+    path: require('./animations/hero.json'),
+  },
 ];
 
 const LottieAnimatedExample = () => {
-
   const [animationIndex, setAnimationIndex] = useState(0);
 
   const file = ANIMATIONS[0];
-  console.log('animations', ANIMATIONS)
-  console.log('file', typeof file)
+  console.log('animations', ANIMATIONS);
+  console.log('file', typeof file);
   // print out the file contents
-  console.log('file', file)
+  console.log('file', file);
 
   const nextAnimation = () => {
     setAnimationIndex((animationIndex + 1) % ANIMATIONS.length);
-  }
+  };
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#fefae0'}}>
       <View style={{alignContent: 'center', alignItems: 'center', padding: 30}}>
-        <Text style={{fontSize: 20, padding: 5, color: '#283618'}}>Animation ({animationIndex + 1} / {ANIMATIONS.length})</Text>
-        <Text style={{fontSize: 24, padding: 5}}>{ANIMATIONS[animationIndex].name}</Text>
+        <Text style={{fontSize: 20, padding: 5, color: '#283618'}}>
+          Animation ({animationIndex + 1} / {ANIMATIONS.length})
+        </Text>
+        <Text style={{fontSize: 24, padding: 5}}>
+          {ANIMATIONS[animationIndex].name}
+        </Text>
         <Button
           onPress={nextAnimation}
           title="Next animation"
           color="#606c38"
         />
       </View>
-      <LottieView source={ANIMATIONS[animationIndex].path} autoPlay loop style={{flex: 1}}/>
+      <LottieView
+        source={ANIMATIONS[animationIndex].path}
+        autoPlay
+        loop
+        style={{flex: 1}}
+      />
     </SafeAreaView>
   );
 };
--- packages/core/src/LottieView/index.web.tsx
+++ packages/core/src/LottieView/index.web.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ViewProps } from "react-native";
+import React from 'react';
+import { ViewProps } from 'react-native';
 import type { LottieViewProps } from '../types';
 import { DotLottiePlayer } from '@dotlottie/react-player';
 import { Player } from '@lottiefiles/react-lottie-player';
@@ -7,36 +7,32 @@ import { Player } from '@lottiefiles/react-lottie-player';
 type Props = LottieViewProps & { containerProps?: ViewProps };
 
 function LottieView(props: Props) {
-    const { source } = props;
-    let isLottie = false;
-    let filename = '';
-    if (typeof source === 'string') {
-        if (source.includes('.lottie')) {
-          isLottie = true;
-        }
-        filename = source;
+  const { source } = props;
+  let isLottie = false;
+  let filename = '';
+  if (typeof source === 'string') {
+    if (source.includes('.lottie')) {
+      isLottie = true;
     }
+    filename = source;
+  }
 
-    if (isLottie) {
-        return (
-            <DotLottiePlayer
-                autoplay={props.autoPlay}
-                loop={props.loop}
-                // @ts-ignore
-                src={props.source}
-                onEvent={(event) => {
-                    console.log('DotLottie', event);
-                }}
-            />
-        );
-    }
+  if (isLottie) {
     return (
-        <Player
+      <DotLottiePlayer
         autoplay={props.autoPlay}
         loop={props.loop}
+        // @ts-ignore
         src={props.source}
-        />
+        onEvent={(event) => {
+          console.log('DotLottie', event);
+        }}
+      />
     );
+  }
+  return (
+    <Player autoplay={props.autoPlay} loop={props.loop} src={props.source} />
+  );
 }
 
 export { LottieView };
